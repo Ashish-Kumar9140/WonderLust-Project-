7736de8b53252cf849641fbfc9271092e9da10c4
Ashish-Kumar9140 <akashish42066@gmail.com>
2026-08-11 21:10:22 +0530

    file manages by express routes

---
+++ routers/listing.js
@@ -0,0 +1,102 @@
+const express = require("express")
+const router = express.Router();
+const wrapAsync = require('../utils/wrapAsync.js');
+const {Listing} = require('../models/listing.js');
+const ExpressError = require('../utils/ExpessError.js');
+const {listingschema} = require('../schema.js');
+
+
+
+function validateListing(req, res, next) {
+    const { error } = listingschema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(msg, 400);
+    }else {
+        next();
+    }
+}
+
+
+// Index route to display all listings
+router.get('/',wrapAsync(async (req, res) => {
+    try {
+        const allListings = await Listing.find({});
+        console.log("listing is done");
+        res.render('listings/index.ejs', {allListings });
+    } catch (err) {
+        console.error("Error fetching listings:", err);
+        res.status(500).send("Internal Server Error");
+    }
+}));
+//adding a new listing
+router.get('/new', (req, res) => {
+    res.render('listings/new.ejs');
+});
+
+
+// Create route to handle form submission and save new listing to the database
+router.post('/', wrapAsync(async (req, res) => {
+    try {
+        const newListing = new Listing(req.body);
+        await newListing.save();
+        res.redirect('/listings');
+        console.log("New listing created successfully");
+    } catch (err) {
+        console.error("Error creating listing:", err);
+        res.status(500).send("Internal Server Error");
+    }
+}));
+
+//edit route to display the edit form for a specific listing by ID
+router.get('/:id/edit',wrapAsync(async (req, res) => {
+    try {
+        const { id } = req.params;
+        const listing = await Listing.findById(id);
+        res.render('listings/edit.ejs', { listing });
+    } catch (err) {
+        console.error("Error fetching listing for edit:", err);
+        res.status(500).send("Internal Server Error");
+    }
+}));
+
+//edit route to handle form submission and update the listing in the database
+router.put('/:id', wrapAsync(async (req, res) => {
+    try {
+        const { id } = req.params;
+        await Listing.findByIdAndUpdate(id, req.body);
+        res.redirect(`/listings/${id}`);
+    } catch (err) {
+        console.error("Error updating listing:", err);
+        res.status(500).send("Internal Server Error");
+    }
+}));
+
+
+ // Delete route to handle deletion of a specific listing by ID
+router.delete('/:id', wrapAsync(async (req, res) => {
+    try {
+        const { id } = req.params;
+        await Listing.findByIdAndDelete(id);
+        res.redirect('/listings');
+        
+    } catch (err) {
+        console.error("Error deleting listing:", err);
+        res.status(500).send("Internal Server Error");
+    }
+}));
+
+
+// show route to display a specific listing by ID
+router.get('/:id',wrapAsync(async (req, res) => {
+    try {
+        const { id } = req.params;
+        const listing = await Listing.findById(id).populate('reviews');
+        res.render('listings/show.ejs', { listing });
+    } catch (err) {
+        console.error("Error fetching listing:", err);
+        res.status(500).send("Internal Server Error");
+    }
+}));
+
+module.exports = router;
---
+++ routers/review.js
@@ -0,0 +1,59 @@
+const express = require("express")
+const router = express.Router({ mergeParams: true });
+const Review = require("../models/reviews");
+const {Listing} = require('../models/listing.js');
+const { model } = require("mongoose");
+const wrapAsync = require('../utils/wrapAsync.js');
+const {listingschema,reviewSchema1} = require('../schema.js');
+const ExpressError = require('../utils/ExpessError.js');
+
+function validateReview(req, res, next) {
+    const { error } = reviewSchema1.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(msg, 400);
+    }else {
+        next();
+    }
+}
+//review route to handle submission of a new review for a specific listing by ID
+//post route to handle submission of a new review for a specific listing by ID
+router.post('/',validateReview,wrapAsync(async (req, res) => {
+    try {
+        const { id } = req.params;
+        const listing = await Listing.findById(id);
+        const newReview = new Review(req.body.review);
+        listing.reviews.push(newReview);
+
+        await newReview.save();
+        await listing.save();
+
+        res.redirect(`/listings/${id}`);
+      
+    } catch (err) {
+        console.error("Error adding review:", err);
+        res.status(500).send("Internal Server Error review post");
+    }
+}));
+
+
+//reviews ka delete route ye hai 
+router.delete("/:reviewId", wrapAsync(async (req ,res)=>{
+    try{
+        let {id , reviewId}= req.params;
+
+    await Listing.findByIdAndUpdate(id ,{$pull : {reviews : reviewId}});
+    await Review.findByIdAndDelete(reviewId);
+
+    res.redirect(`/listings/${id}`);
+
+    }
+    catch{
+         console.error("Error adding review:", err);
+        res.status(500).send("Internal Server Error review delete");
+    }
+    
+}))
+
+module.exports = router;
+
